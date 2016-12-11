c26eb28d4f86f4e8ece670c5ea8a9e12f4b99735
Kevin Amiranoff <kamiranoff@gmail.com>
2016-12-12 00:45:09 +0100

    make proper user of greek-mythology-data package

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var people = require('./data/greeks.json');
 
+var GreekMyth = require('greek-mythology-data');
 
-function compare(a,b) {
-  if (a.name < b.name)
-    return -1;
-  if (a.name > b.name)
-    return 1;
-  return 0;
-}
-
-people.sort(compare);
+var gods = new GreekMyth.Greeks(GreekMyth.gods);
+gods = gods.sortByName();
+var allGreeks = new GreekMyth.Greeks(GreekMyth.all);
+allGreeks = allGreeks.sortByName();
 
 app.get('/api/people',function(req,res){
-  res.send(people);
+  res.send(allGreeks);
+  //assuming your app.js and json file are at same level.
+  //You may change this to 'lib/notes/foo.json' to fit you case
+});
+
+app.get('/api/gods',function(req,res){
+  res.send(gods);
   //assuming your app.js and json file are at same level.
   //You may change this to 'lib/notes/foo.json' to fit you case
 });
