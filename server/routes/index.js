2372fef6e59640b9073bec5db77e74c66a13d64a
Kevin Amiranoff <kamiranoff@gmail.com>
2020-09-13 21:47:22 +0100

    brand new nest app

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-import MythologyRoutes from '../api/mythology/routes/mythology-routes';
-
-export default class Routes {
-   static init(app, router) {
-     MythologyRoutes.init(router);
-
-     app.use('/', router);
-   }
-}
